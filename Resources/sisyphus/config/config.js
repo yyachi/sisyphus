6a103efce7d202a0fe7c82613b2127bd5730b7ef
m-watanabe <m-watanabe@probizmo.co.jp>
2014-03-17 11:20:26 +0900

    新サウンド対応関係

--- Resources/sisyphus/config/config.js
+++ Resources/sisyphus/config/config.js
@@ -2,10 +2,12 @@
 	si.config = {};
 	
 	si.config.Medusa = {
-		defaultServer: 'http://devel.misasa.okayama-u.ac.jp/medusa/',
+        //defaultServer: 'http://192.168.234.141:3000/',
+       defaultServer: 'http://devel.misasa.okayama-u.ac.jp/medusa/',
        //defaultpPrintFormatUrl : 'file:///mnt/sdcard/extStorages/SdCard//okayama.spfmtz',
 		defaultpPrintFormatUrl : 'file:///mnt/sdcard/extStorages/SdCard//okayama.spfmtz',
 		facing: 1, //0 for backward (default) camera, 1 for front camera
+       //debug: true,
 		debug: false,
 		test: false
 	};
