80e843b8925d83465c329012f89cc27ceabb5faa
Kenji Tatano <k-tatano@probizmo.co.jp>
2017-11-16 01:53:05 +0000

    modify label print

--- Resources/sisyphus/ui/LabelPrintSettingWindow.js
+++ Resources/sisyphus/ui/LabelPrintSettingWindow.js
@@ -14,6 +14,13 @@
         });
         win.buttons.Close.left = 0;
 
+        win.buttons.Print = si.ui.createImageButtonView('/images/glyphicons-16-print.png', {
+            width : 90,
+            height : 90,
+            imgDimensions : 30,
+            onclick : function(e) { win.print() }
+        });
+
         win.buttons.Save = si.ui.createImageButtonView('/images/glyphicons-415-disk-save.png', {
             right : 0,
             width : 90,
@@ -110,18 +117,64 @@
 
             Ti.App.Properties.setString('printServer',server.input.value);
             Ti.App.Properties.setString('printFormatUrl',template.input.value);
-            Ti.App.Properties.setString('printerName',printer_name.getSelectedRow(0).title);
-            Ti.App.Properties.setString('printerNameID',printer_name.getSelectedRow(0).id);
-            Ti.App.Properties.setString('templateName',template_name.getSelectedRow(0).title);
-            Ti.App.Properties.setString('templateNameID',template_name.getSelectedRow(0).id);
-            win.close();
 
+            var _printer_name, _printer_id;
+            if (printer_name.getSelectedRow(0) == null) {
+                _printer_name = null;
+                _printer_id = null;
+            } else {
+                _printer_name = printer_name.getSelectedRow(0).title;
+                _printer_id = printer_name.getSelectedRow(0).id;
+            }
+            Ti.App.Properties.setString('printerName',_printer_name);
+            Ti.App.Properties.setString('printerNameID',_printer_id);
+
+            var _template_name, _template_id;
+            if (template_name.getSelectedRow(0) == null) {
+                _template_name = null;
+                _template_id = null;
+            } else {
+                _template_name = template_name.getSelectedRow(0).title;
+                _template_id = template_name.getSelectedRow(0).id;
+            }
+            Ti.App.Properties.setString('templateName',_template_name);
+            Ti.App.Properties.setString('templateNameID',_template_id);
+
+            win.close();
+ 
             if (print_server_bak != Ti.App.Properties.getString('printServer')) {
                Ti.App.Properties.setList("printer_names", null);
                si.app.getPrinterNames();
                Ti.App.Properties.setList("template_names", null);
                si.app.getTemplateNames();
             }
+        };
+
+        win.print = function(){
+            var _message = 'test print...test print...label...';
+            si.app.log.info(_message + 'sending...ok');
+            _message += 'creating...';
+            var _printer_name;
+            if (printer_name.getSelectedRow(0) == null) {
+                _printer_name = null;
+            } else {
+                _printer_name = printer_name.getSelectedRow(0).title;
+            }
+            var _template_name;
+            if (template_name.getSelectedRow(0) == null) {
+                _template_name = null;
+            } else {
+                _template_name = template_name.getSelectedRow(0).title;
+            }
+            si.ui.android.testPrintLabel('test print', 'test print', server.input.value, template.input.value, _printer_name, _template_name, {
+                onsuccess: function(e){
+                    si.sound_label.play();
+                    si.app.log.info(_message + 'ok');
+                },
+                onerror: function(e){
+                    changeMode('error',_message);
+                }
+            });
         };
 
 
@@ -131,25 +184,6 @@
         viewHeader.add(viewHeaderLeft);
         viewHeaderLeft.add(win.buttons.Close);        
         viewHeader.add(viewHeaderRight);
-        win.buttons.Print = si.ui.createImageButtonView('/images/glyphicons-16-print.png', {
-            width : 90,
-            height : 90,
-            imgDimensions : 30,
-            onclick : function(e) {
-                var _message = 'test print...test print...label...';
-                si.app.log.info(_message + 'sending...ok');
-                _message += 'creating...';
-                si.ui.android.testPrintLabel('test print', 'test print', server.input.value, template.input.value, printer_name.getSelectedRow(0).title, template_name.getSelectedRow(0).title, {
-                    onsuccess: function(e){
-                        si.sound_label.play();
-                        si.app.log.info(_message + 'ok');
-                    },
-                    onerror: function(e){
-                        changeMode('error',_message);
-                    }
-                });
-            }
-        });
         viewHeaderRight.add(win.buttons.Print);
         viewHeaderRight.add(win.buttons.Save);
 
